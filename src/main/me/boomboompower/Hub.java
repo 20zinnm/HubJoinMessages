068524fffd1c06ca7cd5aa2a71b4722583b8a442
20zinnm <meyerzinn@gmail.com>
2015-12-30 17:21:10 -0600

    Handle op.
    This should handle op and fix the config.

--- src/main/me/boomboompower/Hub.java
+++ src/main/me/boomboompower/Hub.java
@@ -24,17 +24,22 @@ public class Hub extends JavaPlugin implements Listener {
 	public void onPlayerQuitEvent(PlayerJoinEvent e) {
         // Define the player.
 		Player p = e.getPlayer();
+        // Handle op.
+        if (p.isOp()) {
+            e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("join-messages.op").replace("%player%", p.getName()))); 
+            return;
+        }
         // Loop through all the join messages.
         for (String key: getConfig().getConfigurationSection("join-messages").getKeys(false)) {
             // If the player has the particular permission for the message...
             if (p.hasPermission("hubjoinmessages.join." + key)) {
                 // ... set the join message to the configured one.
-                e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("join-messages." + key))); 
+                e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("join-messages." + key).replace("%player%", p.getName()))); 
                 // Return to leave the loop.
                 return;
             }
             // Otherwise, set the join message to the default.
-            e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("join-messages.default"))); 
+            e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("join-messages.default").replace("%player%", p.getName()))); 
         }
 	}
 	
@@ -42,17 +47,22 @@ public class Hub extends JavaPlugin implements Listener {
 	public void onPlayerQuitEvent(PlayerQuitEvent e) {
         // Define the player.
 		Player p = e.getPlayer();
+        // Handle op.
+        if (p.isOp()) {
+            e.setLeaveMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("leave-messages.op").replace("%player%", p.getName())));
+            return;
+        }
         // Loop through all the leave messages.
 		for (String key: getConfig().getConfigurationSection("leave-messages").getKeys(false)) {
             // If the player has the particular permission for the message...
             if (p.hasPermission("hubjoinmessages.leave." + key)) {
                 // ... set the leave message to the configured one.
-                e.setLeaveMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("leave-messages." + key))); 
+                e.setLeaveMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("leave-messages." + key).replace("%player%", p.getName()))); 
                 // Return to leave the loop.
                 return;
             }
             // Otherwise, set the leave message to the default.
-            e.setLeaveMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("leave-messages.default"))); 
+            e.setLeaveMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("leave-messages.default").replace("%player%", p.getName()))); 
         }
 	}
     
