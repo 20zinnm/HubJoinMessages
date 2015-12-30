c3a268cef7da9387da6d0bf851c5a38db50b3f15
20zinnm <meyerzinn@gmail.com>
2015-12-30 17:11:40 -0600

    Working towards Industry Standards
    I’m trying to make this more to code. It already breaks that because
    the listeners should be in another class, but oh well.

--- src/main/me/boomboompower/Hub.java
+++ src/main/me/boomboompower/Hub.java
@@ -16,100 +16,44 @@ public class Hub extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable() {
+        saveDefaultConfig();
 		Bukkit.getPluginManager().registerEvents(this, this);
 	}
 	
 	@EventHandler
-	public void join(PlayerJoinEvent e) {
+	public void onPlayerQuitEvent(PlayerJoinEvent e) {
+        // Define the player.
 		Player p = e.getPlayer();
-		if (p.hasPermission(getConfig().getString("Elite.Permission"))) e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Elite.JoinMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("Mythical.Permission"))) e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Mythical.JoinMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("Ninja.Permission"))) e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Ninja.JoinMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("Moderator.Permission"))) e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Moderator.JoinMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("Admin.Permission"))) e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin.JoinMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("YouTuber.Permission"))) e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Youtuber.JoinMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("Builder.Permission"))) e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Builder.JoinMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("Developer.Permission"))) e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Developer.JoinMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("Owner.Permission"))) {
-			e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Owner.JoinMessage")).replace("<player>", p.getName()));
-			for (Player all : Bukkit.getOnlinePlayers()) {
-				if (getConfig().getBoolean("Owner.Playsound")) all.playSound(all.getLocation(), Sound.ENDERDRAGON_DEATH, 2, 2);
-				if (getConfig().getBoolean("Owner.PlayTitle")) sendTitle(all, 125, 185, 125, getConfig().getString("Owner.Title"), getConfig().getString("Owner.Subtitle"));
-			}
-		} else {
-			if (getConfig().getBoolean("Default.UseThis")) {
-				e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Default.JoinMessage")).replace("<player>", p.getName()));
-			} else {
-				e.setJoinMessage(null);
-			}
-		}
+        // Loop through all the join messages.
+        for (String key: getConfig().getConfigurationSection("join-messages").getKeys(false)) {
+            // If the player has the particular permission for the message...
+            if (p.hasPermission("hubjoinmessages.join." + key)) {
+                // ... set the join message to the configured one.
+                e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("join-messages." + key))); 
+                // Return to leave the loop.
+                return;
+            }
+            // Otherwise, set the join message to the default.
+            e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("join-messages.default"))); 
+        }
 	}
 	
 	@EventHandler
-	public void quit(PlayerQuitEvent e) {
+	public void onPlayerQuitEvent(PlayerQuitEvent e) {
+        // Define the player.
 		Player p = e.getPlayer();
-		if (p.hasPermission(getConfig().getString("Elite.Permission"))) e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Elite.QuitMessage")).replace("<player>", p.getName()));
-		else if (p.hasPermission(getConfig().getString("Mythical.Permission"))) e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Mythical.QuitMessage")).replace("<player>", p.getName()));
-		else if (p.hasPermission(getConfig().getString("Ninja.Permission"))) e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Ninja.QuitMessage")).replace("<player>", p.getName()));
-		else if (p.hasPermission(getConfig().getString("Moderator.Permission"))) e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Moderator.QuitMessage")).replace("<player>", p.getName()));
-		else if (p.hasPermission(getConfig().getString("Admin.Permission"))) e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin.QuitMessage")).replace("<player>", p.getName()));
-		else if (p.hasPermission(getConfig().getString("YouTuber.Permission"))) e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Youtuber.QuitMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("Builder.Permission"))) e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Builder.QuitMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("Developer.Permission"))) e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Developer.QuitMessage")).replace("<player>", p.getName()));
-		if (p.hasPermission(getConfig().getString("Owner.Permission"))) {
-			e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Owner.QuitMessage")).replace("<player>", p.getName()));
-		} else {
-			if (getConfig().getBoolean("Default.UseThis")) {
-				e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Default.JoinMessage")).replace("<player>", p.getName()));
-			} else {
-				e.setQuitMessage(null);
-			}
-		}
+        // Loop through all the leave messages.
+		for (String key: getConfig().getConfigurationSection("leave-messages").getKeys(false)) {
+            // If the player has the particular permission for the message...
+            if (p.hasPermission("hubjoinmessages.leave." + key)) {
+                // ... set the leave message to the configured one.
+                e.setLeaveMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("leave-messages." + key))); 
+                // Return to leave the loop.
+                return;
+            }
+            // Otherwise, set the leave message to the default.
+            e.setLeaveMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("leave-messages.default"))); 
+        }
 	}
-	
-	private static void sendPacket(Player p, Object packet) {
-		try {
-			Object handle = p.getClass().getMethod("getHandle", new Class[0]).invoke(p, new Object[0]);
-		    Object playerConnection = handle.getClass().getField("playerConnection").get(handle);
-		    playerConnection.getClass().getMethod("sendPacket", new Class[] { getNMSClass("Packet") }).invoke(playerConnection, new Object[] { packet });  
-		} catch (Exception e) {
-			e.printStackTrace();
-		}	
-	}
-	
-	private static Class<?> getNMSClass(String n) {
-		String version = org.bukkit.Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-		try {
-			return Class.forName("net.minecraft.server." + version + "." + n);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace(); 	
-		}
-		return null;
-	}
-	
-	private static void sendTitle(Player p, Integer fadeIn, Integer stay, Integer fadeOut, String title, String subtitle) {
-		try {
-			if (title != null) {
-				title = ChatColor.translateAlternateColorCodes('&', title);
-				title = title.replaceAll("<player>", p.getName());
-				Object enumTitle = getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0].getField("TITLE").get(null);
-				Object chatTitle = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", new Class[] { String.class }).invoke(null, new Object[] { "{\"text\":\"" + title + "\"}" });
-	            Constructor<?> titleConstructor = getNMSClass("PacketPlayOutTitle").getConstructor(new Class[] { getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"), Integer.TYPE, Integer.TYPE, Integer.TYPE });
-	            Object titlePacket = titleConstructor.newInstance(new Object[] { enumTitle, chatTitle, fadeIn, stay, fadeOut });
-	            sendPacket(p, titlePacket);
-			}     
-			if (subtitle != null) {
-	            subtitle = ChatColor.translateAlternateColorCodes('&', subtitle);
-	            subtitle = subtitle.replaceAll("<player>", p.getName());
-	            Object enumSubtitle = getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0].getField("SUBTITLE").get(null);
-	            Object chatSubtitle = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", new Class[] { String.class }).invoke(null, new Object[] { "{\"text\":\"" + subtitle + "\"}" });
-	            Constructor<?> subtitleConstructor = getNMSClass("PacketPlayOutTitle").getConstructor(new Class[] { getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"), Integer.TYPE, Integer.TYPE, Integer.TYPE });
-	            Object subtitlePacket = subtitleConstructor.newInstance(new Object[] { enumSubtitle, chatSubtitle, fadeIn, stay, fadeOut });
-	            sendPacket(p, subtitlePacket); 
-			}
-		} catch (Exception e) {
-			e.printStackTrace();      
-		}
-		
-	}
+    
 }
